1f023d7e05a24932456007e672194328d8ce4a73
Brandon Amos <bamos@cs.cmu.edu>
2016-08-16 08:19:43 -0400

    Remove additional self.saver initialization
    \cc @carpedm20

--- model.py
+++ model.py
@@ -127,7 +127,6 @@ class DCGAN(object):
                           .minimize(self.g_loss, var_list=self.g_vars)
         tf.initialize_all_variables().run()
 
-        self.saver = tf.train.Saver()
         self.g_sum = tf.merge_summary(
             [self.z_sum, self.d__sum, self.G_sum, self.d_loss_fake_sum, self.g_loss_sum])
         self.d_sum = tf.merge_summary(
