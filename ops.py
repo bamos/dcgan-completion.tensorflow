d3dfa795bbf640a469dbecdaa18f85e7dab690db
Fabio Torchetti <mrwho@wedjaa.net>
2017-02-26 09:56:47 -0600

    Fixed tensorflow 1.0 issues - needs retraining.

--- ops.py
+++ ops.py
@@ -21,28 +21,7 @@ class batch_norm(object):
             self.name = name
 
     def __call__(self, x, train=True):
-        shape = x.get_shape().as_list()
-
-        if train:
-            with tf.variable_scope(self.name) as scope:
-                self.beta = tf.get_variable("beta", [shape[-1]],
-                                    initializer=tf.constant_initializer(0.))
-                self.gamma = tf.get_variable("gamma", [shape[-1]],
-                                    initializer=tf.random_normal_initializer(1., 0.02))
-
-                batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
-                ema_apply_op = self.ema.apply([batch_mean, batch_var])
-                self.ema_mean, self.ema_var = self.ema.average(batch_mean), self.ema.average(batch_var)
-
-                with tf.control_dependencies([ema_apply_op]):
-                    mean, var = tf.identity(batch_mean), tf.identity(batch_var)
-        else:
-            mean, var = self.ema_mean, self.ema_var
-
-        normed = tf.nn.batch_norm_with_global_normalization(
-                x, mean, var, self.beta, self.gamma, self.epsilon, scale_after_normalization=True)
-
-        return normed
+        return tf.contrib.layers.batch_norm(x, decay=self.momentum, updates_collections=None, epsilon=self.epsilon, scale=True, scope=self.name)
 
 def binary_cross_entropy(preds, targets, name=None):
     """Computes binary cross entropy given `preds`.
