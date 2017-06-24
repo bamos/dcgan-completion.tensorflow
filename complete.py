02820aa035ff7ffcd4156bab87af07c1f4d18d86
Richard Davies <richard@daviesmail.org>
2017-06-24 20:09:38 +0100

    Offer completion from low resolution averaged pixels

--- complete.py
+++ complete.py
@@ -28,7 +28,7 @@ parser.add_argument('--checkpointDir', type=str, default='checkpoint')
 parser.add_argument('--outDir', type=str, default='completions')
 parser.add_argument('--outInterval', type=int, default=50)
 parser.add_argument('--maskType', type=str,
-                    choices=['random', 'center', 'left', 'full', 'grid'],
+                    choices=['random', 'center', 'left', 'full', 'grid', 'lowres'],
                     default='center')
 parser.add_argument('--centerScale', type=float, default=0.25)
 parser.add_argument('imgs', type=str, nargs='+')
